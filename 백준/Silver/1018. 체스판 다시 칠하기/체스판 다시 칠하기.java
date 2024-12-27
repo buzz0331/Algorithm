07febad0d7c6f7762342e0684d95566dd38f44de
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2024-12-28 00:09:23 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 148 ms, Memory: 19840 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] size = br.readLine().split(" ");
+        int row_size = Integer.parseInt(size[0]);
+        int col_size = Integer.parseInt(size[1]);
+
+        String[] line = new String[row_size];
+        for (int i = 0; i < row_size; i++) {
+            line[i] = br.readLine();
+        }
+
+        int min = Integer.MAX_VALUE; //칠하는 최소 정사각형
+        final int LIMIT = 8;
+
+        boolean isBlack = true; //첫번째가 검정 정사각형인 경우
+        int flag = 0;
+        while (isBlack || flag ==1) {
+            flag++;
+            for (int start_row = 0; start_row < row_size - 7; start_row++) {
+                for (int start_col = 0; start_col < col_size - 7; start_col++) {
+                    int temp = getMin(start_row, start_col, line, isBlack);
+                    if (temp < min) {
+                        min = temp;
+                    }
+                }
+            }
+
+            isBlack = false;
+        }
+
+        System.out.println(min);
+
+    }
+
+    private static int getMin(int start_row, int start_col, String[] line, boolean isBlack) {
+        final String WHITE = "WBWBWBWB";
+        final String BLACK = "BWBWBWBW";
+        int count = 0;
+
+        for (int i = 0; i < 8; i++) { //8x8 정사각형 조회
+            String check = line[start_row+i].substring(start_col, start_col +8);
+            for (int j = 0; j < 8; j++) {
+                count = getCount(isBlack, i, count, BLACK, j, check, WHITE);
+            }
+        }
+
+        return count;
+    }
+
+    private static int getCount(boolean isBlack, int i, int count, String BLACK, int j, String check, String WHITE) {
+        if (i % 2 == 0) {
+            if (isBlack) {
+                count = compareLine(BLACK, j, check, count);
+            } else {
+                count = compareLine(WHITE, j, check, count);
+            }
+        } else {
+            if (isBlack) {
+                count = compareLine(WHITE, j, check, count);
+            } else {
+                count = compareLine(BLACK, j, check, count);
+            }
+        }
+        return count;
+    }
+
+    private static int compareLine(String s, int j, String check, int count) {
+        if (s.charAt(j) != check.charAt(j)) {
+            count++;
+        }
+        return count;
+    }
+}
