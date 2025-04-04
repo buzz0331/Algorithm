ba092f9c7040f105af9a4f462850afa9b3764974
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2025-04-05 03:35:11 +0900

    [Silver III] Title: 소수 구하기, Time: 236 ms, Memory: 19780 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,44 +1,9 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-//
-//public class Main {
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
-//        StringBuilder sb = new StringBuilder();
-//
-//        int M = Integer.parseInt(st.nextToken());
-//        int N = Integer.parseInt(st.nextToken());
-//
-//        for (int i = M; i <= N; i++) {
-//            if (checkPrimeNumber(i, sb) && i != 1) {
-//                sb.append(i).append("\n");
-//            }
-//        }
-//
-//        System.out.println(sb);
-//    }
-//
-//    private static boolean checkPrimeNumber(int i, StringBuilder sb) {
-//        for (int j = 2; j <= (int) Math.sqrt(i); j++) {
-//            if (i % j == 0) {
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
-//}
+import java.io.*;
+import java.util.*;
 
-/**
- * 에라토스테네스의 체
- */
 public class Main {
 
-    private static int[] prime;
-    private static final int PRIME = 0;
-    private static final int NOT_PRIME = 1;
+    private static boolean[] isNotPrime;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -48,27 +13,24 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
 
-        prime = new int[N + 1];
-        prime[1] = NOT_PRIME;
+        isNotPrime = new boolean[N + 1];
+        isNotPrime[0] = true;
+        isNotPrime[1] = true;
 
-        for (int i = 2; i <= N; i++) {
-            if (prime[i] == NOT_PRIME) {
-                continue;
-            }
-
-            if (i >= M) {
-                sb.append(i).append("\n");
-            }
-
-            int primeNumber = i;
-            int multiply = 2;
-            while (primeNumber * multiply <= N) {
-                prime[primeNumber * multiply] = NOT_PRIME;
-                multiply++;
+        for (int i = 2; i * i <= N; i++) {
+            if (!isNotPrime[i]) {
+                for (int j = i * i; j <= N; j += i) {
+                    isNotPrime[j] = true;
+                }
             }
         }
 
-        System.out.println(sb);
+        for (int i = M; i <= N; i++) {
+            if (!isNotPrime[i]) {
+                sb.append(i).append("\n");
+            }
+        }
 
+        System.out.print(sb);
     }
 }
