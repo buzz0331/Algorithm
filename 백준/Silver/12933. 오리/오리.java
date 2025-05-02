bcc794a289c3263601d1ac14193938a0b415c290
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2025-05-03 03:13:48 +0900

    [Silver II] Title: 오리, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/12933. 오리/오리.java
+++ 백준/Silver/12933. 오리/오리.java
@@ -1,85 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 
-    private static final Map<Character, Integer> map = Map.of(
-            'q', 1,
-            'u', 2,
-            'a', 3,
-            'c', 4,
-            'k', 5);
-    private static final ArrayList<Integer> ducks = new ArrayList<>();
-
     public static void main(String[] args) throws IOException {
         boolean isSuccess = true;
+        int[] stage = new int [5]; // 'q', 'u', 'a', 'c', 'k'
+        int currentDucks = 0;   //현재 울고 있는 오리
+        int maxDucks = 0; //동시에 울 수 있는 최대 오리 수
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char[] alphabets = br.readLine().toCharArray();
 
         for (char alphabet : alphabets) {
-            switch(alphabet) {
-                case 'q':
-                    if (checkDucks('k', 'q')) {
-                        continue;
-                    } else {
-                        ducks.add(map.get('q'));
-                        break;
-                    }
-                case 'u':
-                    if (checkDucks('q', 'u')) {
-                        continue;
-                    }
-                    isSuccess = false;
-                    break;
-
-                case 'a':
-                    if (checkDucks('u', 'a')) {
-                        continue;
-                    }
-                    isSuccess = false;
-                    break;
-                case 'c':
-                    if (checkDucks('a', 'c')) {
-                        continue;
-                    }
-                    isSuccess = false;
-                    break;
-                case 'k':
-                    if (checkDucks('c', 'k')) {
-                        continue;
-                    }
-                    isSuccess = false;
-                    break;
-            }
-        }
-
-        for (Integer duck : ducks) {
-            if (duck != map.get('k')) {
+            int idx = "quack".indexOf(alphabet);
+            if (idx == -1) {
                 isSuccess = false;
+                break;
+            }
+
+            if (idx == 0) {
+                stage[0]++;
+                currentDucks++;
+                maxDucks = Math.max(maxDucks, currentDucks);
+            } else {
+                if (stage[idx - 1] == 0) {
+                    isSuccess = false;
+                    break;
+                }
+                stage[idx - 1]--;
+                stage[idx]++;
+                if (idx == 4) {
+                    currentDucks--;
+                    stage[idx]--;
+                }
             }
         }
 
-        if (isSuccess) {
-            System.out.println(ducks.size());
-        } else {
-            System.out.println(-1);
+        for (int s : stage) {
+            if (s != 0) {
+                isSuccess = false;
+                break;
+            }
         }
+
+        System.out.println(isSuccess? maxDucks : -1);
 
 
     }
 
-    private static boolean checkDucks(char prev, char curr) {
-        for (int i = 0; i < ducks.size(); i++) {
-            if (ducks.get(i) == map.get(prev)) {
-                ducks.set(i, map.get(curr));
-                return true;
-            }
-        }
-        return false;
-    }
+
 }
