c90f67f3ed017d4591cf626f460bc615e19a702d
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2025-05-17 15:56:57 +0900

    [Gold V] Title: 최소비용 구하기, Time: 396 ms, Memory: 50488 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int N, M;
+    private static ArrayList<Node>[] graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        graph = new ArrayList[N + 1];
+        for(int i = 0; i <= N; i++){
+            graph[i] = new ArrayList<>();
+        }
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            graph[Integer.parseInt(st.nextToken())].add(
+                    new Node(Integer.parseInt(st.nextToken()),
+                    Integer.parseInt(st.nextToken())));
+        }
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        int[] dist = dijkstra(start, end);
+        bw.write(String.valueOf(dist[end]));
+        bw.flush(); bw.close();
+    }
+
+    private static int[] dijkstra(int start, int end) {
+        int[] dist = new int[N + 1];
+        boolean[] visited = new boolean[N + 1];
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
+        pq.offer(new Node(start, 0));
+
+        while(!pq.isEmpty()){
+            Node current = pq.poll();
+            int currentIdx = current.index;
+
+            if(visited[currentIdx]) continue;
+            visited[currentIdx] = true;
+
+            if(currentIdx == end) break;
+
+            for(Node neighbor : graph[currentIdx]){
+                if (dist[neighbor.index] > dist[currentIdx] + neighbor.cost) {
+                    pq.offer(new Node(neighbor.index, dist[currentIdx] + neighbor.cost));
+                    dist[neighbor.index] = dist[currentIdx] + neighbor.cost;
+                }
+            }
+        }
+
+        return dist;
+    }
+
+    private static class Node implements Comparable<Node> {
+        public int index;
+        public int cost;
+
+        public Node(int index, int cost) {
+            this.index = index;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
+        }
+    }
+}
