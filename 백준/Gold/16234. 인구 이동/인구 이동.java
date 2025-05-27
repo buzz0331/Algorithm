06b95cbb00dc972446af496e11e7f2c2aad8439f
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2025-05-27 13:26:58 +0900

    [Gold IV] Title: 인구 이동, Time: 552 ms, Memory: 299656 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -2,89 +2,89 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static int N, L, R;
-    private static int[][] map;
+    private static final int[][] dxy = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    private static int totalPeople;
     private static boolean[][] visited;
-    private static int[] dx = {-1, 1, 0, 0};
-    private static int[] dy = {0, 0, -1, 1};
+    private static ArrayList<Position> union;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        L = Integer.parseInt(st.nextToken());
-        R = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken()); // N x N
+        L = Integer.parseInt(st.nextToken()); // 인구차이 L명 이상
+        R = Integer.parseInt(st.nextToken()); // R명 이하
 
-        map = new int[N][N];
-        for(int i = 0; i < N; i++){
+        int[][] map = new int[N][N];
+
+        for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < N; j++){
+            for(int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int days = 0;
-        while(true){
-            visited = new boolean[N][N];
-            boolean moved = false;
+        int count = 0;
 
-            for(int i = 0; i < N; i++){
-                for(int j = 0; j < N; j++){
-                    if(!visited[i][j]){
-                        if(bfs(i, j))
-                            moved = true;
+        while(true) {
+            boolean isMove = false;
+            visited = new boolean[N][N];
+
+            for(int i = 0; i < N; i++) {
+                for(int j = 0; j < N; j++) {
+                    if(visited[i][j]) continue;
+                    totalPeople = 0;        //같은 연합의 총 인구수
+                    union = new ArrayList<>(); //같은 연합의 나라 리스트
+                    checkMove(i, j, map, 0);
+
+                    if(union.size() == 1) {
+                        continue;
                     }
+
+                    int average = totalPeople / union.size();
+                    for(Position p : union) {
+                        map[p.row][p.col] = average;
+                    }
+                    isMove = true;
                 }
             }
 
-            if(!moved) break;
-            days++;
+            if(!isMove) {
+                break;
+            }
+
+            count++;
         }
 
-        System.out.println(days);
+        System.out.println(count);
     }
 
-    private static boolean bfs(int x, int y){
-        Queue<int[]> q = new LinkedList<>();
-        List<int[]> union = new ArrayList<>();
+    private static void checkMove(int row, int col, int[][] map, int count) {
+        visited[row][col] = true;
+        union.add(new Position(row, col));
+        totalPeople += map[row][col];
+        for (int i = 0; i < dxy.length; i++) {
+            int moveRow = row + dxy[i][0];  //움직일 행
+            int moveCol = col + dxy[i][1];  //움직일 열
 
-        q.offer(new int[]{x, y});
-        union.add(new int[]{x, y});
-        visited[x][y] = true;
-
-        int sum = map[x][y];
-
-        while(!q.isEmpty()){
-            int[] curr = q.poll();
-            int cx = curr[0];
-            int cy = curr[1];
-
-            for(int d = 0; d < 4; d++){
-                int nx = cx + dx[d];
-                int ny = cy + dy[d];
-
-                if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-                if(visited[nx][ny]) 
-                    continue;
-
-                int diff = Math.abs(map[cx][cy] - map[nx][ny]);
-                if(diff >= L && diff <= R){
-                    visited[nx][ny] = true;
-                    q.offer(new int[]{nx, ny});
-                    union.add(new int[]{nx, ny});
-                    sum += map[nx][ny];
-                }
+            if(moveRow < 0 || moveCol < 0 || moveRow == N || moveCol == N) continue;
+            if(visited[moveRow][moveCol]) continue;
+            int subtract = Math.abs(map[row][col] - map[moveRow][moveCol]);
+            if (subtract <= R && subtract >= L) {
+                count++;
+                checkMove(moveRow, moveCol, map, count);
             }
         }
+    }
 
-        if(union.size() == 1) return false;
+    private static class Position {
+        public int row;
+        public int col;
 
-        int avg = sum / union.size();
-        for(int[] pos : union){
-            map[pos[0]][pos[1]] = avg;
+        public Position(int row, int col) {
+            this.row = row;
+            this.col = col;
         }
-
-        return true;
     }
 }
