d2ced7c3ccaa33650ab90bc9f2bae87b58b02ff7
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2025-07-01 17:24:00 +0900

    [Silver I] Title: 포도주 시식, Time: 132 ms, Memory: 15968 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -33,19 +33,14 @@ public class Main {
         dp[0] = table[0];
         dp[1] = table[0] + table[1];
 
-        for(int i = 2; i < table.length; i++) {
-            dp[i] = Math.max(findMaxDp(i) + table[i], table[i - 1] + table[i]); // 두 칸 왼쪽 수 연속된 수 중 최대 선택
-            table[i] += findMaxDp(i); // 두 칸 왼쪽 수는 뒤 점화식에 영향을 주지 않으므로 영구적으로 업데이트
+        int[] maxDpUntil = new int[table.length];
+        maxDpUntil[0] = dp[0];
+        maxDpUntil[1] = Math.max(dp[0], dp[1]);
+
+        for (int i = 2; i < table.length; i++) {
+            dp[i] = Math.max(maxDpUntil[i - 2] + table[i], table[i - 1] + table[i]);
+            table[i] += maxDpUntil[i - 2];
+            maxDpUntil[i] = Math.max(maxDpUntil[i - 1], dp[i]);
         }
-    }
-
-    private static int findMaxDp(int index) {
-        int maxDp = -1;
-
-        for(int i = index - 2; i >= 0; i--) {
-            maxDp = Math.max(maxDp, dp[i]);
-        }
-
-        return maxDp;
     }
 }
