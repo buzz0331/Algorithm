dc39e3351b09b9c0779b0c06de6b8aa6bbee9f13
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2025-07-14 02:59:03 +0900

    [Gold V] Title: LCS, Time: 132 ms, Memory: 18520 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,25 +1,25 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s1 = br.readLine();
         String s2 = br.readLine();
 
         int n = s1.length();
         int m = s2.length();
 
-        // dp[i][j] = s1의 i번째까지, s2의 j번째까지의 LCS 길이
         int[][] dp = new int[n + 1][m + 1];
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1; // 문자가 같을 때
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]); // 다를 때
+                if(s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
@@ -27,3 +27,4 @@ public class Main {
         System.out.println(dp[n][m]);
     }
 }
+
