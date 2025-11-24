ba0cc63e8f2890c3ade861f8fe677f5916271c3c
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2025-11-24 16:06:47 +0900

    [Gold II] Title: 보석 도둑, Time: 1156 ms, Memory: 131608 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -4,15 +4,16 @@ import java.io.*;
 public class Main {
 
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static int N, K;
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-        Jewel[] jewels = new Jewel[N];
-        int[] bags = new int[K];
+        PriorityQueue<Jewel> jewels = new PriorityQueue<>();
+        TreeMap<Integer, Integer> bagMap = new TreeMap<>(); // 가방 용량, 갯수
 
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -20,35 +21,33 @@ public class Main {
             int m = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
 
-            jewels[i] = new Jewel(m, v);
+            jewels.add(new Jewel(m, v));
         }
 
         for(int i = 0; i < K; i++) {
-            bags[i] = Integer.parseInt(br.readLine());
+            int key = Integer.parseInt(br.readLine());
+            bagMap.put(key, bagMap.getOrDefault(key, 0) + 1);
         }
 
-        Arrays.sort(jewels, Comparator.comparingInt(j -> j.weight)); // 보석 무게 기준으로 오름차순 정렬
-        Arrays.sort(bags); // 가방 용량으로 오름차순 정렬
-
-        System.out.println(findMaxValue(jewels, bags));
+        System.out.println(findMaxValue(jewels, bagMap));
     }
 
-    private static long findMaxValue(Jewel[] jewels, int[] bags) {
-        PriorityQueue<Jewel> max = new PriorityQueue<>();
+    private static long findMaxValue(PriorityQueue<Jewel> jewels, TreeMap<Integer, Integer> bagMap) {
         long sum = 0;
-        int pointer = 0;
+        int count = 0; // 가방에 들어간 보석 갯수
 
-        for(int i = 0; i < bags.length; i++) {
-            int capacity = bags[i];
+        while(!jewels.isEmpty()) {
+            if(count == K) break;
+            Jewel jewel = jewels.poll();
 
-            while(pointer < jewels.length && capacity >= jewels[pointer].weight) { // 해당 가방에 들어갈 수 있는 모든 보석들을 우선순위 큐에 삽입
-                max.offer(jewels[pointer]);
-                pointer++;
-            }
+            Integer capacity = bagMap.ceilingKey(jewel.weight); // 현재 보석 무게 이상의 용량을 가진 가방 중 가장 가까운 수
+            if(capacity == null) continue; // 만족하는 가방이 없음
 
-            if(max.isEmpty()) continue;
+            Integer num = bagMap.get(capacity); // 가방 갯수
+            if(--num == 0) bagMap.remove(capacity);
+            else bagMap.put(capacity, num);
 
-            Jewel jewel = max.poll(); // 현재 가방에 들어갈 수 있는 보석 중 가치가 가장 높은 보석 선택
+            count++;
             sum += jewel.value;
         }
         return sum;
