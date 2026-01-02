2b6af58be3b97f207f077576b8dc366b798176b7
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2026-01-02 16:12:43 +0900

    [Gold V] Title: 빗물, Time: 104 ms, Memory: 14492 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -10,8 +10,6 @@ public class Main {
         int H = Integer.parseInt(st.nextToken());
         int W = Integer.parseInt(st.nextToken());
 
-        int answer = 0;
-
         int[] blocks = new int[W];
         st = new StringTokenizer(br.readLine());
 
@@ -19,23 +17,24 @@ public class Main {
             blocks[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i = 1; i < W - 1; i++) { // 인덱스 별 모이는 빗물
-            int left = 0;
-            int right = 0;
-
-            for(int j = 0; j < i; j++) {
-                left = Math.max(blocks[j], left);
-            }
-
-            for(int j = i + 1; j < W; j++) {
-                right = Math.max(blocks[j], right);
-            }
-
-            if(blocks[i] < left && blocks[i] < right) answer += Math.min(left, right) - blocks[i];
-
+        int[] leftMax = new int[W];
+        int[] rightMax = new int[W];
+        
+        leftMax[0] = blocks[0];
+        for(int i = 1; i < W; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], blocks[i]);
         }
 
-        System.out.print(answer);
+        rightMax[W - 1] = blocks[W - 1];
+        for(int i = W - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], blocks[i]);
+        }
 
+        int waterTrapped = 0;
+        for(int i = 0; i < W; i++) {
+            waterTrapped += Math.max(0, Math.min(leftMax[i], rightMax[i]) - blocks[i]);
+        }
+
+        System.out.print(waterTrapped);
     }
 }
