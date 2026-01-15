2cf20c8d7e63e58cce4f7fb4ee8813be8a502398
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2026-01-15 17:49:35 +0900

    [Gold IV] Title: 플로이드, Time: 360 ms, Memory: 42788 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -4,75 +4,43 @@ import java.io.*;
 public class Main {
 
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static List<Bus>[] bus;
-    private static int n, m;
-
-    private static StringBuilder sb = new StringBuilder();
+    private static int n, m, INF = 1_000_000_000;;
+    private static int[][] dist;
 
     public static void main(String[] args) throws IOException {
         n = Integer.parseInt(br.readLine());
         m = Integer.parseInt(br.readLine());
 
-        bus = new ArrayList[n + 1];
-        for(int i = 1; i < bus.length; i++) {
-            bus[i] = new ArrayList<>();
+        dist = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
         }
-
-        for(int i = 0; i < m; i++) {
+        for(int i = 1; i <= m; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-
-            bus[from].add(new Bus(to, cost));
+            dist[from][to] = Math.min(dist[from][to], cost);
         }
 
-        for(int i = 1; i < n + 1; i++) {
-            dijkstra(i);
-        }
-        System.out.print(sb);
-    }
-
-    private static void dijkstra(int start) {
-        int[] dist = new int[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        PriorityQueue<Bus> pq = new PriorityQueue<>();
-        dist[start] = 0;
-        pq.offer(new Bus(start, 0));
-
-        while(!pq.isEmpty()) {
-            Bus current = pq.poll();
-
-            for(Bus next: bus[current.to]) {
-                int newCost = current.cost + next.cost;
-
-                if(dist[next.to] > newCost){
-                    dist[next.to] = newCost;
-                    pq.offer(new Bus(next.to, newCost));
+        for(int k = 1; k <= n; k++) { // 중간 노드 설정
+            for(int i = 1; i <= n; i++) { // 최단거리 측정하는 두 노드 설정
+                for(int j = 1; j <= n; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
+
         }
 
-        for(int i = 1; i < dist.length; i++) {
-            if(dist[i] == Integer.MAX_VALUE) sb.append(0).append(" ");
-            else sb.append(dist[i]).append(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                sb.append(dist[i][j] == INF ? 0 : dist[i][j]).append(" ");
+            }
+            sb.append("\n");
         }
 
-        sb.append("\n");
-    }
-
-    private static class Bus implements Comparable<Bus> {
-        public int to;
-        public int cost;
-
-        Bus(int to, int cost) {
-            this.to = to;
-            this.cost = cost;
-        }
-
-        @Override
-        public int compareTo(Bus o) {
-            return Integer.compare(this.cost, o.cost);
-        }
+        System.out.print(sb);
     }
 }
