a4213d258dedf25b53243ea083f427c26d3b5f67
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2026-01-22 10:57:34 +0900

    [D2] Title: 나무 높이, Time: 104 ms, Memory: 26,240 KB -BaekjoonHub

---
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+class Solution
+{
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String args[]) throws Exception
+    {
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            sb.append("#").append(test_case).append(" ");
+           int N = Integer.parseInt(br.readLine());
+           StringTokenizer st = new StringTokenizer(br.readLine());
+           int[] trees = new int[N];
+           int max = -1;
+
+           for(int i = 0; i < N; i++) {
+               trees[i] = Integer.parseInt(st.nextToken());
+               max = Math.max(max, trees[i]);
+           }
+
+           int count1 = 0, count2 = 0;
+           for(int i = 0; i < N; i++) {
+               int gap = max - trees[i];
+                count2 += gap / 2;
+                count1 += gap % 2;
+           }
+
+           while(count2 > count1 + 1) {
+               count2--;
+               count1 += 2;
+           }
+
+           if(count1 > count2) {
+               sb.append(count1 * 2 - 1);
+           } else if(count1 == count2) {
+               sb.append(count2 * 2);
+           } else {
+               sb.append(count2 * 2);
+           }
+
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
