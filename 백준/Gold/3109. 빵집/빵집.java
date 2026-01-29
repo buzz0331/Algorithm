4ae77073740d3b0fb02c36bd7b5a0e2d9dca6ca2
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2026-01-29 14:03:05 +0900

    [Gold II] Title: 빵집, Time: 868 ms, Memory: 324188 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -7,8 +7,7 @@ public class Main {
 			{-1, 1}, {0, 1}, {1, 1}
 	};
 	private static int[][] map;
-	private static int[][] visited;
-	private static int R, C, max = -1;
+	private static int R, C;
 	private static int count;
 
 	public static void main(String[] args) throws IOException{
@@ -20,7 +19,6 @@ public class Main {
 		C = Integer.parseInt(st.nextToken());
 		
 		map = new int[R][C];
-		visited = new int[R][C]; // 0 : 아예 방문 x / 1 : 해당 라운드 방문 중 / 2 : 파이프라인이 방문 확정
 		
 		for(int i = 0; i < R; i++) {
 			String[] s = br.readLine().split("");
@@ -29,7 +27,6 @@ public class Main {
 			}
 		}
 
-		
 		// 파이프라인 시작 위치
 		for(int i = 0; i < R; i++) {
 			if(dfs(i,0)) count++;
@@ -42,27 +39,29 @@ public class Main {
 	
 	private static boolean dfs(int row, int col) {
 		if(col == C - 1) {
-			visited[row][col] = 2;
+			map[row][col] = 2;
 //			print();
 			return true;
 		}
 		
-		visited[row][col] = 1;
 //		print();
 		
 		for(int[] direction : directions) {
 			int nR = row + direction[0];
 			int nC = col + direction[1];
 			
-			if(isOut(nR, nC) || visited[nR][nC] != 0 || map[nR][nC] == 1) continue;
+			if(isOut(nR, nC)) continue;
 			
-			boolean result = dfs(nR, nC);
-			if(result) { // 파이프라인 성공
-				visited[row][col] = 2;
-				return true;
-			} else {
-				visited[row][col] = 1;
-				continue;
+			if(map[nR][nC] == 0) {
+				if(nC == C - 1) {
+					return true;
+				}
+				
+				map[nR][nC] = 2;
+				
+				if(dfs(nR, nC)) {
+					return true;
+				}
 			}
 		}
 		
@@ -76,7 +75,7 @@ public class Main {
 	
 	private static void print() {
 		for(int i = 0; i < R; i++) {
-			System.out.println(Arrays.toString(visited[i]));
+			System.out.println(Arrays.toString(map[i]));
 			
 		}System.out.println();
 	}
