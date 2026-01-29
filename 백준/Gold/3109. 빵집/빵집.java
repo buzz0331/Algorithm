f59a42b28261c95691a52f9983143fb3e70279fa
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2026-01-30 08:55:50 +0900

    [Gold II] Title: 빵집, Time: 404 ms, Memory: 59512 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -7,9 +7,8 @@ public class Main {
 			{-1, 1}, {0, 1}, {1, 1}
 	};
 	private static int[][] map;
-	private static int[][] visited;
+	private static boolean[][] visited;
 	private static int R, C, max = -1;
-	private static int count;
 
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
@@ -20,7 +19,7 @@ public class Main {
 		C = Integer.parseInt(st.nextToken());
 		
 		map = new int[R][C];
-		visited = new int[R][C]; // 0 : 아예 방문 x / 1 : 해당 라운드 방문 중 / 2 : 파이프라인이 방문 확정
+		visited = new boolean[R][C];
 		
 		for(int i = 0; i < R; i++) {
 			char[] c = br.readLine().toCharArray();
@@ -29,10 +28,12 @@ public class Main {
 			}
 		}
 		
+		int count = 0;
+		
 		// 파이프라인 시작 위치
 		for(int i = 0; i < R; i++) {
-			if(dfs(i,0)) count++;
 			
+			if(dfs(i, 0)) count++;
 		}
 		
 		System.out.print(count);
@@ -41,31 +42,22 @@ public class Main {
 	
 	private static boolean dfs(int row, int col) {
 		if(col == C - 1) {
-			visited[row][col] = 2;
-//			print();
 			return true;
 		}
 		
-		visited[row][col] = 1;
-//		print();
+		visited[row][col] = true;
 		
 		for(int[] direction : directions) {
 			int nR = row + direction[0];
 			int nC = col + direction[1];
 			
-			if(isOut(nR, nC) || visited[nR][nC] != 0 || map[nR][nC] == 1) continue;
+			if(isOut(nR, nC) || visited[nR][nC] || map[nR][nC] == 1) continue;
 			
-			boolean result = dfs(nR, nC);
-			if(result) { // 파이프라인 성공
-				visited[row][col] = 2;
+			if(dfs(nR, nC)) {
 				return true;
-			} else {
-				visited[row][col] = 1;
-				continue;
-			}
+			} 
 		}
 		
-//		visited[row][col] = 0;
 		return false;
 	}
 	
@@ -73,11 +65,5 @@ public class Main {
 		return row < 0 || col < 0 || row >= R || col >= C;
 	}
 	
-	private static void print() {
-		for(int i = 0; i < R; i++) {
-			System.out.println(Arrays.toString(visited[i]));
-			
-		}System.out.println();
-	}
-
+	
 }
