d452bc8151dc5129da784f682f5d0997cdc1235b
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2026-02-03 17:20:23 +0900

    [Silver II] Title: 참외밭, Time: 60 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.java
+++ 백준/Silver/2477. 참외밭/참외밭.java
@@ -1,70 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    private static final int[] length = new int[6];
-    private static int size;
-
-    private static int maxWidthIdx = 0;
-    private static int maxHeightIdx = 0;
-
-    private static int maxWidth = 0;
-    private static int maxHeight = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int K = Integer.parseInt(br.readLine());
-        StringTokenizer st;
+        int value = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < 6; i++) {
-            st = new StringTokenizer(br.readLine());
-            int direction = Integer.parseInt(st.nextToken());
-            int distance = Integer.parseInt(st.nextToken());
+        ArrayList<Integer> list = new ArrayList<>();
+        int maxWidth = -1, maxHeight = -1;
+        for(int i = 0; i < 6; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            if ((direction == 1 || direction == 2) && maxWidth < distance){
-                maxWidthIdx = i;
-                maxWidth = distance;
-            } else if ((direction == 3 || direction == 4) && maxHeight < distance) {
-                maxHeightIdx = i;
-                maxHeight = distance;
+            if(a == 3 || a == 4) { // 높이
+                maxHeight = Math.max(maxHeight, b);
+            } else {
+                maxWidth = Math.max(maxWidth, b);
             }
 
-            length[i] = distance;
+            list.add(b);
         }
 
-        int rightIdx, leftIdx;
-        if (maxWidthIdx + 1 == 6) {
-            rightIdx = 0;
-        } else {
-            rightIdx = maxWidthIdx + 1;
+        int sum = 0;
+        list.add(list.get(0));
+        for(int i = 0; i < 6; i++) {
+            sum += list.get(i) * list.get(i + 1);
         }
-        if (maxWidthIdx - 1 == -1) {
-            leftIdx = 5;
-        } else {
-            leftIdx = maxWidthIdx - 1;
-        }
-        int width = Math.abs(length[rightIdx] - length[leftIdx]);
 
-        if (maxHeightIdx + 1 == 6) {
-            rightIdx = 0;
-        } else {
-            rightIdx = maxHeightIdx + 1;
-        }
-        if (maxHeightIdx - 1 == -1) {
-            leftIdx = 5;
-        } else {
-            leftIdx = maxHeightIdx - 1;
-        }
-        int height = Math.abs(length[rightIdx] - length[leftIdx]);
+        int extent = maxWidth * maxHeight;
+        int answer = sum - 2 * extent;
 
-        size = maxWidth * maxHeight - (width * height);
-
-        System.out.println(size * K);
+        System.out.print(answer * value);
     }
-
 
 }
