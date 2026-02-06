9787f96ebd80d133f77fed8a7d4c1e048c6f78e2
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2026-02-06 14:07:33 +0900

    [Gold V] Title: 치킨 배달, Time: 148 ms, Memory: 13872 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,112 +1,71 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    private static final int HOUSE = 1;
-    private static final int CHICKEN = 2;
-
-    private static final ArrayList<Position> chickens = new ArrayList<>();
-    private static final ArrayList<Position> chooseChickens = new ArrayList<>();
-    private static final ArrayList<Position> houses = new ArrayList<>();
-
-    private static boolean[] visited;
-
-    private static int N;
-    private static int M;
-
+    private static List<int[]> chickens = new ArrayList<>(), houses = new ArrayList<>();
+    private static int N, M;
+    private static int[] selectedChickens;
     private static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+       StringTokenizer st = new StringTokenizer(br.readLine());
+       N = Integer.parseInt(st.nextToken());
+       M = Integer.parseInt(st.nextToken());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+       selectedChickens = new int[M];
 
-        /**
-         * 각 치킨, 집 리스트 초기화
-         */
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                assignType(st, i, j);
-            }
-        }
-        visited = new boolean[chickens.size()];
-        for (int i = 0; i < chickens.size(); i++) {
-            startDfs(i);
-        }
+       for(int i = 0; i < N; i++) {
+           st = new StringTokenizer(br.readLine());
+           for(int j = 0; j < N; j++) {
+               int temp = Integer.parseInt(st.nextToken());
+               if(temp == 2) chickens.add(new int[]{i, j});
+               else if(temp == 1) houses.add(new int[]{i, j});
+           }
+       }
 
+        selectChickens(0, 0);
+       System.out.print(min);
 
-        System.out.println(min);
     }
 
-    private static void dfs(int currentChicken) {
-        if (chooseChickens.size() == M) {    // M개의 조합을 완성했다면
-            // 치킨거리 계산 시작
-            calculateChickenDistance();
+    private static void selectChickens(int depth, int start) {
+        if(depth == M) {
+            calChickenDistance();
+//            System.out.println(Arrays.toString(selectedChickens));
             return;
         }
 
-        for (int i = currentChicken + 1; i < chickens.size(); i++) {
-            if (!visited[i]) {
-                startDfs(i);
-            }
+        for(int i = start; i < chickens.size(); i++) {
+            selectedChickens[depth] = i;
+            selectChickens(depth + 1, i + 1);
         }
     }
 
-    private static void startDfs(int i) {
-        visited[i] = true;
-        chooseChickens.add(chickens.get(i));
-        dfs(i);
-        visited[i] = false;
-        chooseChickens.remove(chooseChickens.size() - 1);
-    }
+    private static void calChickenDistance() {
+        int[] distances = new int[houses.size()];
+        Arrays.fill(distances, Integer.MAX_VALUE);
+        for(int s : selectedChickens) {
+            int[] chickenLocation = chickens.get(s);
+            for(int i = 0; i < houses.size(); i++) {
+                int[] houseLocation = houses.get(i);
 
-    private static void calculateChickenDistance() {
+                int distance = Math.abs(chickenLocation[0] - houseLocation[0]) + Math.abs(chickenLocation[1] - houseLocation[1]);
+                distances[i] = Math.min(distances[i], distance);
+//                System.out.println(Arrays.toString(distances));
+            }
+        }
+
+
+
         int sum = 0;
-        int chickenDistance;
-        for (Position house : houses) {
-            chickenDistance = Integer.MAX_VALUE;
-            for (Position chicken : chooseChickens) {
-                // 가장 최소인 치킨거리 탐색
-                chickenDistance = Math.min(Math.abs(house.X - chicken.X) + Math.abs(house.Y - chicken.Y), chickenDistance);
-            }
-
-            if (min < chickenDistance) {
-                // 이미 현재 최소 치킨거리를 넘으면 백트래킹
-                return;
-            }
-
-            sum += chickenDistance;
+        for(int d : distances) {
+            sum += d;
         }
 
         min = Math.min(min, sum);
     }
 
-
-    private static void assignType(StringTokenizer st, int i, int j) {
-        int type = Integer.parseInt(st.nextToken());
-        if (type == CHICKEN) {
-            chickens.add(new Position(i, j));
-        }
-        if (type == HOUSE) {
-            houses.add(new Position(i, j));
-        }
-    }
-
-    private static class Position {
-        public int X;
-        public int Y;
-
-        public Position(int x, int y) {
-            X = x;
-            Y = y;
-        }
-    }
 }
