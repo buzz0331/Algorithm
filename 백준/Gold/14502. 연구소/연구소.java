b83ef483ce118ec5daeb69f40d0bb212ec2e0b56
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2026-02-10 16:43:03 +0900

    [Gold IV] Title: 연구소, Time: 724 ms, Memory: 294780 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,115 +1,84 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    static final int dx[] = {0,0,1,-1};  //상하좌우 방향 설정
-    static final int dy[] = {1,-1,0,0};  //상화좌우 방향 설정
-    static int originalMap[][];
-    static int n,m;
-    static int maxSafeZone = Integer.MIN_VALUE; //최대값을 찾기 위한 최소값 설정
+    private static int[][] map;
+    private static List<int[]> viruses = new ArrayList<>();
+    private static int N, M;
+    private static int maxArea = -1, wallCnt = 3;
+
+    private static int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        originalMap = new int[n][m];
-
-        for(int i=0; i<n; i++) {
+        map = new int[N][M];
+        for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<m; j++) {
-                originalMap[i][j] = Integer.parseInt(st.nextToken());
+            for(int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if(map[i][j] == 2) viruses.add(new int[]{i, j}); // 바이러스 위치
+                if(map[i][j] == 1) wallCnt++;
             }
         }
 
-        dfs(0);
+        setWall(0, 0, 0);
+        System.out.print(maxArea);
 
-        System.out.println(maxSafeZone);
     }
 
-    static void dfs(int wallCnt) {
-        //벽이 3개가 설치 된 경우 bfs 탐색 시작
-        if(wallCnt == 3) {
-            bfs();
+    private static void setWall(int row, int col, int depth) {
+        if(depth == 3) {
+            int virusCount = spreadVirus();
+            int safeArea = (N * M) - wallCnt - virusCount;
+            maxArea = Math.max(maxArea, safeArea);
+//            for (int i = 0; i < N; i++) System.out.println(Arrays.toString(map[i]));
+//            System.out.println();
             return;
         }
 
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(originalMap[i][j] == 0) {
-                    originalMap[i][j] = 1;
-                    dfs(wallCnt+1);
-                    originalMap[i][j] = 0;
-                }
+        for(int i = 0; i < N * M; i++) {
+            int r = i / M;
+            int c = i % M;
+            if(map[r][c] == 0) {
+                map[r][c] = 1;
+                setWall(r, c, depth + 1);
+                map[r][c] = 0;
             }
         }
     }
 
-    static void bfs() {
-        Queue<Node> q = new LinkedList<>();
+    private static int spreadVirus() {
+        Queue<int[]> queue = new ArrayDeque<>();
+        boolean[][] visited = new boolean[N][M];
+        int count = 0;
+        for(int[] virus : viruses) {
+            queue.offer(virus);
+            visited[virus[0]][virus[1]] = true;
+            count++;
+        }
 
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(originalMap[i][j] == 2) {
-                    q.add(new Node(i,j));
-                }
+        while(!queue.isEmpty()) {
+            int[] current = queue.poll();
+
+            for(int[] direction : directions) {
+                int nR = current[0] + direction[0];
+                int nC = current[1] + direction[1];
+
+                if(nR < 0 || nC < 0 || nR >= N || nC >= M) continue;
+                if(visited[nR][nC] || map[nR][nC] != 0) continue;
+
+                visited[nR][nC] = true;
+                queue.offer(new int[]{nR, nC});
+                count++;
             }
         }
 
-        //원본 연구소를 바꾸지 않기 위한 카피맵 사용
-        int copyMap[][] = new int[n][m];
-
-        for (int i = 0; i < n; i++) {
-            copyMap[i] = originalMap[i].clone();
-        }
-
-        //BFS 탐색 시작
-        while(!q.isEmpty()) {
-            Node now = q.poll();
-            int x = now.x; // 현재 값
-            int y = now.y; //
-
-            for(int k=0; k<4; k++) {
-                int nx = x + dx[k];
-                int ny = y + dy[k];
-
-                //연구소 범위 밖이 아니고 빈칸일 경우에만 바이러스를 퍼트린다.
-                if(0<=nx && nx<n && 0<=ny && ny<m) {
-                    if(copyMap[nx][ny] == 0) {
-                        q.add(new Node(nx,ny));
-                        copyMap[nx][ny] = 2;
-                    }
-                }
-            }
-        }
-
-        //SafeZone 확인
-        funcSafeZone(copyMap);
-    }
-
-    private static void funcSafeZone(int[][] copyMap) {
-        int safeZone =0;
-        for(int i=0; i<n ; i++) {
-            for(int j=0; j<m; j++) {
-                if(copyMap[i][j] == 0) {
-                    safeZone++;
-                }
-            }
-        }
-        if (maxSafeZone < safeZone) {
-            maxSafeZone = safeZone;
-        }
-    }
-
-    //Queue에 좌표값 x,y를 넣기 위함.
-    static class Node {
-        int x;
-        int y;
-        Node(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
+        return count;
     }
 }
