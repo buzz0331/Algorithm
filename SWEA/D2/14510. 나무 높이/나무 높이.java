8249c9ec81df715063e7380c9afb64a9dd15878c
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2026-02-11 11:48:46 +0900

    [D2] Title: 나무 높이, Time: 100 ms, Memory: 26,240 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -13,32 +13,33 @@ class Solution
         for(int test_case = 1; test_case <= T; test_case++)
         {
             sb.append("#").append(test_case).append(" ");
-           int N = Integer.parseInt(br.readLine());
-           StringTokenizer st = new StringTokenizer(br.readLine());
-           int[] trees = new int[N];
-           int max = -1;
+            int N = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] trees = new int[N];
+            int max = -1;
 
-           for(int i = 0; i < N; i++) {
-               trees[i] = Integer.parseInt(st.nextToken());
-               max = Math.max(max, trees[i]);
-           }
+            for(int i = 0; i < N; i++) {
+                trees[i] = Integer.parseInt(st.nextToken());
+                max = Math.max(max, trees[i]);
+            }
 
-           int count1 = 0, count2 = 0;
-           for(int i = 0; i < N; i++) {
-               int gap = max - trees[i];
+            int count1 = 0, count2 = 0;
+            for(int i = 0; i < N; i++) {
+                int gap = max - trees[i];
                 count2 += gap / 2;
                 count1 += gap % 2;
-           }
+            }
 
-            // count1이 더 많아질 때까지 반복
-           while(count2 > count1 + 1) {
-               count2--;
-               count1 += 2;
-           }
-            
-            if(count1 > count2) { // 홀수가 더 많은 경우
+            while(count2 > count1 + 1) {
+                count2--;
+                count1 += 2;
+            }
+
+            if(count1 > count2) {
                 sb.append(count1 * 2 - 1);
-            } else { // 짝수가 더 많은 경우
+            } else if(count1 == count2) {
+                sb.append(count2 * 2);
+            } else {
                 sb.append(count2 * 2);
             }
 
