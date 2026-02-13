b81ebd01a057095c055a4fd358d5f74e6d691431
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2026-02-13 13:59:51 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 68 ms, Memory: 12620 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,84 +1,71 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    private static int[] sequence;
-    private static int[] op = new int[4];
-
-    private static long max = Long.MIN_VALUE;
-    private static long min = Long.MAX_VALUE;
+    private static int[] numbers, op = new int[4];
+    private static int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+    private static int[] p;
     private static int N;
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
+        numbers = new int[N];
+        p = new int[N - 1];
 
-        sequence = new int[N + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
-            sequence[i] = Integer.parseInt(st.nextToken()); //수열 초기화
+        for(int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 4; i++) {
+        for(int i = 0; i < 4; i++) {
             op[i] = Integer.parseInt(st.nextToken());
         }
 
-        dfs(1, sequence[1], op);
-
-        StringBuilder sb = new StringBuilder();
-
-        sb.append(max).append("\n");
-        sb.append(min).append("\n");
-        System.out.println(sb);
+        backTracking(0);
+        System.out.println(max);
+        System.out.println(min);
     }
 
-    private static void dfs(int currentSeq, long sum, int[] op) {
-
-        for (int i = 0; i < 4; i++) {
-            int[] temp = op.clone();
-            if (temp[i] == 0) {
-                continue;
-            } else {
-                temp[i] -= 1;
-            }
-
-            long result = operation(i, sum, sequence[currentSeq + 1]);   //현재 결과 연산
-
-
-            currentSeq += 1;
-
-            if (currentSeq == N) {
-                max = Math.max(result, max);
-                min = Math.min(result, min);
-                return;
-            }
-
-            dfs(currentSeq, result, temp);
-            currentSeq -= 1;
+    private static void backTracking(int depth) {
+        if(depth == N - 1) {
+            calculate();
+            return;
         }
 
+        for(int i = 0; i < 4; i++) {
+            if(op[i] <= 0) continue;
+
+            op[i]--;
+            p[depth] = i;
+            backTracking(depth + 1);
+            op[i]++;
+        }
     }
 
-    private static long operation(int opIndex, long op1, int op2) {
+    private static void calculate() {
+        int result = numbers[0];
+        for(int i = 0; i < N - 1; i++) {
+            int num = numbers[i + 1];
+            switch(p[i]) {
+                case 0: // 덧셈
+                    result += num;
+                    break;
+                case 1: // 뺄셈
+                    result -= num;
+                    break;
+                case 2: // 곱셈
+                    result *= num;
+                    break;
+                case 3: // 나눗셈
+                    result /= num;
+                    break;
+            }
+        }
 
-        if (opIndex == 0) {
-            return op1 + op2;
-        }
-        if (opIndex == 1) {
-            return op1 - op2;
-        }
-        if (opIndex == 2) {
-            return op1 * op2;
-        }
-        if (opIndex == 3) {
-            return op1 / op2;
-        }
-
-        return 0;
+        max = Math.max(max, result);
+        min = Math.min(min, result);
     }
 }
