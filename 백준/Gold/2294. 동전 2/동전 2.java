f27d47d65ba262f6c4e69dfc99dadbbf0ac69b2a
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2026-02-27 14:33:35 +0900

    [Gold V] Title: 동전 2, Time: 80 ms, Memory: 11720 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    
+
     private static final int INF = 10_000_000;
 
     public static void main(String[] args) throws IOException {
@@ -10,31 +10,22 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
+        int[] coins = new int[n];
+        int[] dp = new int[k + 1]; // x원을 얻기 위한 동전 최소 개수
 
-        Set<Integer> coinSet = new HashSet<>();
-        List<Integer> coins = new ArrayList<>();
         for(int i = 0; i < n; i++) {
-            int c = Integer.parseInt(br.readLine());
-            if(!coinSet.contains(c)) {
-                coins.add(c);
-                coinSet.add(c);
-            }
+            coins[i] = Integer.parseInt(br.readLine());
         }
 
-        long[] dp = new long[k + 1]; // x원을 얻기 위한 동전 최소 개수
         Arrays.fill(dp, INF);
         dp[0] = 0;
 
-        for(int i = 1; i <= k; i++) {
-            for(int c : coins) {
-                if(i - c >= 0) dp[i] = Math.min(dp[i], dp[i - c] + 1);
+        for(int coin : coins) {
+            for(int j = coin; j <= k; j++) {
+                dp[j] = Math.min(dp[j], dp[j - coin] + 1);
             }
         }
 
         System.out.print(dp[k] == INF ? -1 : dp[k]);
     }
 }
-
-//2 8
-//8
-//19
