7567a57b2ddc012edbd09e14a9ef44c24a4fecdc
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2026-03-13 10:56:48 +0900

    [Gold IV] Title: 플로이드, Time: 304 ms, Memory: 44040 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -2,45 +2,50 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	
+	private static int[][] dist;
+	private static final int INF = 10000000;
 
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static int n, m, INF = 1_000_000_000;;
-    private static int[][] dist;
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		
+		dist = new int[n + 1][n + 1];
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= n; j++) {
+				if(i == j) continue;
+				dist[i][j] = INF;
+			}
+		}
+		
+		for(int i = 0; i < m; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			dist[a][b] = Math.min(dist[a][b], c);
+		}
+		
+		for(int k = 1; k <= n; k++) {
+			for(int i = 1; i <= n; i++) {
+				for(int j = 1; j <= n; j++) {
+					dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+				}
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= n; j++) {
+				sb.append(dist[i][j] == INF ? 0 : dist[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+	}
 
-    public static void main(String[] args) throws IOException {
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
-
-        dist = new int[n + 1][n + 1];
-        for (int i = 1; i <= n; i++) {
-            Arrays.fill(dist[i], INF);
-            dist[i][i] = 0;
-        }
-        for(int i = 1; i <= m; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            dist[from][to] = Math.min(dist[from][to], cost);
-        }
-
-        for(int k = 1; k <= n; k++) { // 중간 노드 설정
-            for(int i = 1; i <= n; i++) { // 최단거리 측정하는 두 노드 설정
-                for(int j = 1; j <= n; j++) {
-                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
-                }
-            }
-
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= n; j++) {
-                sb.append(dist[i][j] == INF ? 0 : dist[i][j]).append(" ");
-            }
-            sb.append("\n");
-        }
-
-        System.out.print(sb);
-    }
 }
