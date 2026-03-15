cf6adaed53f656cf2862266fc1a2f38004a26187
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2026-03-15 16:03:11 +0900

    [Gold IV] Title: 운동, Time: 464 ms, Memory: 58580 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.java
+++ 백준/Gold/1956. 운동/운동.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class Main {
 
-    private static List<Edge>[] adj;
+    private static int[][] dist;
     private static final int INF = 400000000;
     private static int V, E;
 
@@ -13,9 +13,9 @@ public class Main {
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
 
-        adj = new ArrayList[V + 1];
+        dist = new int[V + 1][V + 1];
         for(int i = 1; i <= V; i++) {
-            adj[i] = new ArrayList<>();
+            Arrays.fill(dist[i], INF);
         }
 
         for(int i = 0; i < E; i++) {
@@ -24,52 +24,33 @@ public class Main {
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-            adj[a].add(new Edge(b, c));
+            dist[a][b] = c;
         }
 
-        int min = INF;
-        for(int i = 1; i <= V; i++) {
-            int result = dijkstra(i);
-
-            if(result != 0) {
-                min = Math.min(min, result);
-            }
-        }
-
-        System.out.print(min == INF ? -1 : min);
+        floydWarshall();
+        System.out.print(findMinCycle());
     }
 
-    private static int dijkstra(int start) {
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        int[] dist = new int[V + 1];
-
-        Arrays.fill(dist, INF);
-        pq.offer(new Edge(start, 0));
-        dist[start] = 0;
-        int count = 0;
-
-        while(!pq.isEmpty()) {
-            Edge current = pq.poll();
-
-//            System.out.println("current: " + current.to);
-
-            if(dist[current.to] < current.cost) continue;
-
-            if(current.to == start) {
-                if(++count == 2) break;
-            }
-
-            for(Edge next : adj[current.to]) {
-                int newDist = dist[current.to] + next.cost;
-
-                if(dist[next.to] == 0 || dist[next.to] > newDist) { // 사이클 조건 추가
-                    dist[next.to] = newDist;
-                    pq.offer(new Edge(next.to, newDist));
+    private static void floydWarshall() {
+        for(int k = 1; k <= V; k++) {
+            for(int i = 1; i <= V; i++) {
+                for(int j = 1; j <= V; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
-        return dist[start];
     }
+
+    private static int findMinCycle() {
+        int min = INF;
+        for(int i = 1; i <= V; i++) {
+            min = Math.min(min, dist[i][i]);
+        }
+
+        return min == INF ? -1 : min;
+    }
+
+
 
     private static class Edge implements Comparable<Edge> {
         public int to, cost;
