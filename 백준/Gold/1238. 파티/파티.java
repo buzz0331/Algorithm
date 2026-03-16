f27fed696284ba1afe7d8707e76032738710e6f7
Jang Hyun Jun <130340360+buzz0331@users.noreply.github.com>
2026-03-16 17:21:06 +0900

    [Gold III] Title: 파티, Time: 140 ms, Memory: 17096 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,89 +1,94 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class Main{
+	
+	private static int N, M;
+	
+	private static final int INF = 100_000_000;
+	
+	public static void main(String[] args) throws IOException {
+		//------여기에 솔루션 코드를 작성하세요.------------
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+		
+		List<Edge>[] graph = new ArrayList[N + 1];
+		List<Edge>[] reverseGraph = new ArrayList[N + 1];
+		
+		for(int i = 1; i <= N; i++) {
+			graph[i] = new ArrayList<>();
+			reverseGraph[i] = new ArrayList<>();
+		}
+		
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			
+			graph[a].add(new Edge(b, cost));
+			reverseGraph[b].add(new Edge(a, cost));
+		}
+		
+		int[] dist = dijkstra(X, graph);
+		int[] reverseDist = dijkstra(X, reverseGraph);
+		
+		int max = -1;
+		for(int i = 1; i <= N; i++) {
+			int to = dist[i];
+			int from = reverseDist[i];
+			
+			if(to == INF || from == INF) continue; // 갔다가 되돌아오는 경로가 없는 경우
+			max = Math.max(max, to + from);
+		}
+		
+		System.out.print(max);
+		
+	}
+	
+	private static int[] dijkstra(int start, List<Edge>[] graph) {
+		int[] dist = new int[N + 1];
+		Arrays.fill(dist, INF);
+		
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		
+		pq.offer(new Edge(start, 0));
+		dist[start] = 0;
 
-    private static List<Edge>[] graph, reverseGraph;
-    private static int N, M, X;
+		while(!pq.isEmpty()) {
+			Edge current = pq.poll();
+			
+			if(dist[current.to] < current.cost) continue;
+			
+			for(Edge next : graph[current.to]) {
+				int newDist = dist[current.to] +  next.cost;
+				if(dist[next.to] > newDist) {
+					dist[next.to] = newDist;
+					pq.offer(new Edge(next.to, newDist));
+				}
+			}
+		}
+		
+		return dist;
+	}
+	
+	private static class Edge implements Comparable<Edge> {
+		public int to, cost;
+		
+		Edge(int to, int cost) {
+			this.to = to;
+			this.cost = cost;
+		}
+		
+		@Override
+		public int compareTo(Edge o) {
+			return Integer.compare(this.cost, o.cost);
+		}
+	}
+	
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        X = Integer.parseInt(st.nextToken());
-
-        graph = new ArrayList[N + 1];
-        reverseGraph = new ArrayList[N + 1];
-
-        for(int i = 1; i <= N; i++) {
-            graph[i] = new ArrayList<>();
-            reverseGraph[i] = new ArrayList<>();
-        }
-
-        for(int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-
-            graph[x].add(new Edge(y, cost));
-            reverseGraph[y].add(new Edge(x, cost));
-        }
-
-        int[] go = dijkstra(graph);
-        int[] back = dijkstra(reverseGraph);
-
-        int max = -1;
-        for(int start = 1; start <= N; start++) {
-            if(start == X) continue;
-
-            max = Math.max(max, go[start] + back[start]);
-        }
-
-        System.out.print(max);
-    }
-
-    private static int[] dijkstra(List<Edge>[] graph) {
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        boolean[] visited = new boolean[N + 1];
-
-        int[] dist = new int[N + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-
-        dist[X]= 0;
-        pq.offer(new Edge(X, 0));
-
-        while(!pq.isEmpty()) {
-            Edge current = pq.poll();
-
-            if(visited[current.to]) continue;
-            visited[current.to] = true;
-
-            for(Edge next : graph[current.to]) {
-                int newDist = dist[current.to] + next.cost;
-                if(dist[next.to] > newDist) {
-                    dist[next.to] = newDist;
-                    pq.offer(new Edge(next.to, newDist));
-                }
-            }
-        }
-        return dist;
-    }
-
-    private static class Edge implements Comparable<Edge> {
-        public int to;
-        public int cost;
-
-        public Edge(int to, int cost) {
-            this.to = to;
-            this.cost = cost;
-        }
-
-        @Override
-        public int compareTo(Edge o) {
-            return Integer.compare(this.cost, o.cost);
-        }
-    }
 }
